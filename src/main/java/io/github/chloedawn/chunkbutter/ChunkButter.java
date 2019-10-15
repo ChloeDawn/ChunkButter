0b159dcd61850288493496a22bed332aa03cf76d
Chloe Dawn <chloealexisdawn@outlook.com>
2019-10-15 19:17:51 +0100

    Update project

--- src/main/java/io/github/chloedawn/chunkbutter/ChunkButter.java
+++ src/main/java/io/github/chloedawn/chunkbutter/ChunkButter.java
@@ -1,7 +1,9 @@
-package io.github.sleeplessdevelopment.chunkbutter.mixin;
+package io.github.chloedawn.chunkbutter;
 
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.render.chunk.ChunkRenderer;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
@@ -11,23 +13,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ChunkRenderer.class)
 abstract class ChunkRendererMixin {
-  private ChunkRendererMixin() {
-    throw new AssertionError();
-  }
-
+  @Contract(mutates = "param")
   @Inject(method = "shouldRebuildOnClientThread", at = @At("RETURN"), cancellable = true)
-  private void chunkbutter$forceRebuildOffMainThread(final CallbackInfoReturnable<Boolean> cir) {
+  private void chunkbutter$forceRebuildOffMainThread(final @NotNull CallbackInfoReturnable<Boolean> cir) {
     cir.setReturnValue(false);
   }
 }
 
 @Mixin(WorldRenderer.class)
 abstract class WorldRendererMixin {
-  private WorldRendererMixin() {
-    throw new AssertionError();
-  }
-
-  @ModifyConstant(method = "setUpTerrain", constant = @Constant(doubleValue = 768.0))
+  @Contract(pure = true)
+  @ModifyConstant(method = "setUpTerrain", constant = @Constant(doubleValue = 768.0), allow = 1)
   private double chunkbutter$reduceMinimumRange(final double range) {
     return 0.0;
   }
